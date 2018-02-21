2d2b9edc9fb879d79d5860eb29985a507ca15264
Reid Laitinen <reidlaitinen@gmail.com>
2018-02-21 08:46:19 -0700

    add about page and route

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var methodOverride = require('method-override');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var synths = require('./routes/synths');
+var about = require('./routes/about');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/synths', synths);
+app.use('/about', about);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
